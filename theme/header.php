962b2449eb7e3f32972584c8649718dda56a9fda
Jan Dh <jan.dhollander84@gmail.com>
2015-11-03 17:14:14 +0100

    Update header.php
    A small custom check to auto-include some of the og:tags.

--- theme/header.php
+++ theme/header.php
@@ -16,11 +16,48 @@
 	<meta name="theme-color" content="#ffffff">
 
 	<?php // mobile meta ?>
-	<meta name="HandheldFriendly" content="True">
-	<meta name="MobileOptimized" content="320">
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+
+	<?php //A check to auto-include some og:tags in your header, depending on what type of template you are on ?>
+	if (is_singular( 'post')) {
+
+      $name = get_the_title(); 
+      $post = $wp_query->post;
+      $id = $post->ID;
+      $p = get_post($id);
+      $text = $p->post_content;
+      $excerpt_length = apply_filters( 'excerpt_length', 55 );  <?php //adjust excerpt length to what you want ?>
+      $text = wp_trim_words( $text, $excerpt_length);
+      $imgUrl = wp_get_attachment_thumb_url(get_post_thumbnail_id($id));
+      $link = get_permalink(); 
+ 
+      echo '<meta property="og:url" content ="'. $link . '" />
+            <meta property="og:title" content="'. $name . '" /> 
+            <meta property="og:description" content="'. $text . '" />
+            <meta property="og:image" content="' . $imgUrl . '" />
+            <meta property="og:type" content="article" />';
+    }
+    else
+    {
+      $name = get_bloginfo('name' );
+      $description = get_bloginfo('description');
+      $imgUrl = get_template_directory_uri();
+      $link = "<?php //fill in the url of your website here ?>" ;
+
+
+      echo '<meta property="og:url" content ="'. $link . '" />
+            <meta property="og:title" content="'. $name . '" />
+            <meta property="og:description" content="' . $description . '" />
+            <meta property="og:image" content="' . $imgUrl . '/library/images/logo-fb.png" />  <?php //this should point to the folder where your images are ?>
+            <meta property="og:type" content="website" />';
+    }
+  ?>
+  <meta property="og:image:width" content="250" />
+  <meta property="og:image:height" content="250" />
+
+
 
 	<?php wp_head(); ?>
 </head>
